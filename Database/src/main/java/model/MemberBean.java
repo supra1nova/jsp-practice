5892f44490c80e1790672f3fbe5a4fad87c550d3
supra1nova <supra1nova@gmail.com>
2022-10-07 19:09:19 +0900

    #22-23강 - 데이터베이스 및 신규 프로젝트, MemberBean.java 및 MemberJoin.jsp 생성 회원가입 페이지를 생성하고 db 연동하는 실습을 위해 프로젝트와 필요 파일들을 일부 생성함.

---
+++ Database/src/main/java/model/MemberBean.java
@@ -0,0 +1,71 @@
+package model;
+
+public class MemberBean {
+
+	private String id;
+	private String pass1;
+	private String pass2;
+	private String email;
+	private String tel;
+	private String hodbby;
+	private String job;
+	private String age;
+	private String infi;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPass1() {
+		return pass1;
+	}
+	public void setPass1(String pass1) {
+		this.pass1 = pass1;
+	}
+	public String getPass2() {
+		return pass2;
+	}
+	public void setPass2(String pass2) {
+		this.pass2 = pass2;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getHodbby() {
+		return hodbby;
+	}
+	public void setHodbby(String hodbby) {
+		this.hodbby = hodbby;
+	}
+	public String getJob() {
+		return job;
+	}
+	public void setJob(String job) {
+		this.job = job;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	public String getInfi() {
+		return infi;
+	}
+	public void setInfi(String infi) {
+		this.infi = infi;
+	}
+	
+	
+}
