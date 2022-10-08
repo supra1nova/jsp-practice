c43177bc1499216249390abbe2cd03bc3d2e915a
supra1nova <supra1nova@gmail.com>
2022-10-09 00:06:29 +0900

    * DB table 생성eclipse 신규 프로젝스 생성 및 server.xml 설정, 빌드 위한 더미 jsp 페이지 작성, BoardBean.java 생성

---
+++ Board/src/main/java/model/BoardBean.java
@@ -0,0 +1,87 @@
+package model;
+
+import java.time.LocalDate;
+
+public class BoardBean {
+	private Integer num;
+	private String writer;
+	private String email;
+	private String subject;
+	private String password;
+	private LocalDate reg_date;
+	private Integer ref;
+	private Integer re_step;
+	private Integer re_level;
+	private Integer readcount;
+	private String content;
+	
+	public Integer getNum() {
+		return num;
+	}
+	public void setNum(Integer num) {
+		this.num = num;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public LocalDate getReg_date() {
+		return reg_date;
+	}
+	public void setReg_date(LocalDate reg_date) {
+		this.reg_date = reg_date;
+	}
+	public Integer getRef() {
+		return ref;
+	}
+	public void setRef(Integer ref) {
+		this.ref = ref;
+	}
+	public Integer getRe_step() {
+		return re_step;
+	}
+	public void setRe_step(Integer re_step) {
+		this.re_step = re_step;
+	}
+	public Integer getRe_level() {
+		return re_level;
+	}
+	public void setRe_level(Integer re_level) {
+		this.re_level = re_level;
+	}
+	public Integer getReadcount() {
+		return readcount;
+	}
+	public void setReadcount(Integer readcount) {
+		this.readcount = readcount;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	
+
+}
